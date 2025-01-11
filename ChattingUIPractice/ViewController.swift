7f10289e706524dc9482e66c0983e56ad73eece2
BAE <lexon1209@gmail.com>
2025-01-11 18:10:38 +0900

    Initial Commit

---
+++ ChattingUIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChattingUIPractice
+//
+//  Created by BAE on 1/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
